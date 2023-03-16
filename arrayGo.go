2c5b118704f1ae8cc571e1ab59bc3c3f51bbe7b8
berlin <berlinalexander31@gmail.com>
2023-03-16 10:29:31 -0700

    Update arrayGo.go

--- arrayGo.go
+++ arrayGo.go
@@ -1,21 +1,37 @@
 package main
+
 import "fmt"
 
-func main(){
-	var a[5] int
-	a[0]=101
-	a[1]=102
-	a[2]=103
-	a[3]=104
-	a[4]=105
-	fmt.Print(a)
-    var sum int
-   for i=0;i<5;i++
-   {
-	sum+=a[i]
-   }
-   fmt.Println(sum)
+func main() {
+	var car [5]int
+	car[0] = 0
+	car[1] = 1
+	car[2] = 2
+	car[3] = 3
+	car[4] = 4
+	fmt.Println(car)
+	fmt.Println(len(car))
+	bike := [3]string{"BMW", "duke", "eee"}
+	fmt.Println(len(bike))
+	bike2 := &bike
+	fmt.Println(len(bike2))
+	car1 := []int{1, 2, 3, 4}
+	fmt.Println(len(car1))
+	fmt.Println(cap(car1))
+	car3 := car1[:]
+	car4 :=
+		car1[2:]
+	car5 :=
+		car1[:2]
+	fmt.Println(car3)
+	fmt.Println(car4)
+	fmt.Println(car5)
+	hello := []int{}
+	hello = append(hello, 1)
+	fmt.Println(len(hello))
+	fmt.Println(cap(hello))
+	hello = append(hello, 2, 3, 4, 5)
+	fmt.Println(len(hello))
+	fmt.Println(cap(hello))
 
-   var twodim[1][4]int
-   
 }
