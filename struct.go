f72f9b9ecbdce060dc0b601b5fd0de084460a66d
berlin <berlinalexander31@gmail.com>
2023-03-16 20:34:45 -0700

    Update struct.go

--- struct.go
+++ struct.go
@@ -1,9 +1,25 @@
 package main
-import "fmt"
+import {
+	"fmt"
+	"reflect"
+}
 type netflix struct{
 	 Number int
 	 Name string 
 	 Naration [] string 
+}
+type animal struct{
+	name string
+	location string
+}
+type bird struct{
+	animal
+	speed int
+	canfly bool
+}
+type animal1 struct{
+	name string 'required max="100"'
+	origin string
 }
 func main()
 {
@@ -27,4 +43,26 @@ func main()
 	}
 	fmt.Println(netflix1)
 	fmt.Println(netflix1.Naration)
+	aname:=struct{name string}{name:berlin}//anonymous struct
+	anotheraname:=aname
+	anotheraname.name=rookie
+	fmt.Println(anotheraname)
+	fmt.Println(aname)
+	anotheraname:=&aname //pointer  to the aname 
+	anotheraname.name=rookie
+	fmt.Println(anotheraname) 
+	fmt.Println(aname)//it change to the aname
+
+	b:=bird{}
+	b.name="emu"
+	b.location="australia"
+	b.speed="125"
+	b.canfly=false
+	fmt.Print(b.name)
+    t:=reflect.TypeOf(animal1{})
+	field,_:=t.FieldByname{"name"}
+	fmt.Println(field.Tag)
+   
+
+     
 }
